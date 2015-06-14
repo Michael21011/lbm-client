f1ee9143c40b9834d6e9ad6e517161ac3205ff7e
Oren Sokolowsky <orens@lazooz.org>
2015-06-14 16:17:38 +0300

    Connect RideShare Activity

--- lbm/src/main/java/com/lazooz/lbm/MainActivity.java
+++ lbm/src/main/java/com/lazooz/lbm/MainActivity.java
@@ -142,7 +142,7 @@ public class MainActivity extends MyActionBarActivity  {
         View.OnClickListener TrainRideShareListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TrainRideShareActivity.class);
+                Intent intent = new Intent(MainActivity.this, RideShareEnterRequestActivity.class);
                 startActivity(intent);
             }
         };
